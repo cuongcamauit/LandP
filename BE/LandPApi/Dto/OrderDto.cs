089074b86da8a4bc7c088875124cd157e3a5a4d2
cuongcamauit <cuongcamauit@gmail.com>
2023-05-01 22:01:16 +0700

    add paidat to orderdto

--- BE/LandPApi/Dto/OrderDto.cs
+++ BE/LandPApi/Dto/OrderDto.cs
@@ -9,7 +9,8 @@ namespace LandPApi.Dto
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public Status Status { get; set; }
         public DateTime Date { get; set; }
-        public bool isPaid { get; set; }
+        //public bool isPaid { get; set; }
+        public DateTime? PaidAt { get; set; }
         public double Total { get; set; }
         public string? CustomerId { get; set; }
     }
